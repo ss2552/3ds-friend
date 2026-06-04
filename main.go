8c5adcc5bd36a8572857162d92732bbe77fe04ad
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-05 02:25:25 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"sync"
 
-	"github.com/PretendoNetwork/friends/grpc"
 	"github.com/PretendoNetwork/friends/nex"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	wg.Add(3)
+	wg.Add(2)
 
-	go grpc.StartGRPCServer()
 	go nex.StartAuthenticationServer()
 	go nex.StartSecureServer()
 
