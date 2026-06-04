dca2477ba444738cbfceddb8b1af374307f813c5
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-05 03:16:42 +0900

    Update register_secure_server_protocols.go

--- nex/register_secure_server_protocols.go
+++ nex/register_secure_server_protocols.go
@@ -4,38 +4,16 @@ import (
 	"github.com/PretendoNetwork/friends/globals"
 	nex_account_management "github.com/PretendoNetwork/friends/nex/account-management"
 	nex_friends_3ds "github.com/PretendoNetwork/friends/nex/friends-3ds"
-	nex_friends_wiiu "github.com/PretendoNetwork/friends/nex/friends-wiiu"
 	account_management "github.com/PretendoNetwork/nex-protocols-go/v2/account-management"
 	friends_3ds "github.com/PretendoNetwork/nex-protocols-go/v2/friends-3ds"
-	friends_wiiu "github.com/PretendoNetwork/nex-protocols-go/v2/friends-wiiu"
 )
 
 func registerSecureServerProtocols() {
 	accountManagementProtocol := account_management.NewProtocol()
-	friendsWiiUProtocol := friends_wiiu.NewProtocol()
 	friends3DSProtocol := friends_3ds.NewProtocol()
 
 	// * Account Management protocol handles
 	accountManagementProtocol.NintendoCreateAccount = nex_account_management.NintendoCreateAccount
-
-	// * Friends (WiiU) protocol handles
-	friendsWiiUProtocol.UpdateAndGetAllInformation = nex_friends_wiiu.UpdateAndGetAllInformation
-	friendsWiiUProtocol.AddFriendRequest = nex_friends_wiiu.AddFriendRequest
-	friendsWiiUProtocol.RemoveFriend = nex_friends_wiiu.RemoveFriend
-	friendsWiiUProtocol.CancelFriendRequest = nex_friends_wiiu.CancelFriendRequest
-	friendsWiiUProtocol.AcceptFriendRequest = nex_friends_wiiu.AcceptFriendRequest
-	friendsWiiUProtocol.DeleteFriendRequest = nex_friends_wiiu.DeleteFriendRequest
-	friendsWiiUProtocol.DenyFriendRequest = nex_friends_wiiu.DenyFriendRequest
-	friendsWiiUProtocol.MarkFriendRequestsAsReceived = nex_friends_wiiu.MarkFriendRequestsAsReceived
-	friendsWiiUProtocol.AddBlackList = nex_friends_wiiu.AddBlackList
-	friendsWiiUProtocol.RemoveBlackList = nex_friends_wiiu.RemoveBlackList
-	friendsWiiUProtocol.UpdatePresence = nex_friends_wiiu.UpdatePresence
-	friendsWiiUProtocol.UpdateComment = nex_friends_wiiu.UpdateComment
-	friendsWiiUProtocol.UpdatePreference = nex_friends_wiiu.UpdatePreference
-	friendsWiiUProtocol.GetBasicInfo = nex_friends_wiiu.GetBasicInfo
-	friendsWiiUProtocol.DeletePersistentNotification = nex_friends_wiiu.DeletePersistentNotification
-	friendsWiiUProtocol.CheckSettingStatus = nex_friends_wiiu.CheckSettingStatus
-	friendsWiiUProtocol.GetRequestBlockSettings = nex_friends_wiiu.GetRequestBlockSettings
 
 	// * Friends (3DS) protocol handles
 	friends3DSProtocol.UpdateProfile = nex_friends_3ds.UpdateProfile
@@ -55,6 +33,5 @@ func registerSecureServerProtocols() {
 	friends3DSProtocol.GetAllFriends = nex_friends_3ds.GetAllFriends
 
 	globals.SecureEndpoint.RegisterServiceProtocol(accountManagementProtocol)
-	globals.SecureEndpoint.RegisterServiceProtocol(friendsWiiUProtocol)
 	globals.SecureEndpoint.RegisterServiceProtocol(friends3DSProtocol)
 }
