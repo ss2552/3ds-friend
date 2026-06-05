130aa3e5beaf823bbb51ffe8eec218313dba0379
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-05 16:09:58 +0900

    Remove unused GRPC fields from config struct
    Removed unused GRPC configuration fields from the config struct.

--- globals/config.go
+++ globals/config.go
@@ -4,14 +4,9 @@ type config struct {
 	PostgresURI              string
 	PostgresMaxConnections   int64
 	AESKey                   string
-	GRPCAPIKey               string `envconf:"optional"`
-	GRPCServerPort           uint16
 	AuthenticationServerPort uint16
 	SecureServerHost         string
 	SecureServerPort         uint16
-	AccountGRPCHost          string
-	AccountGRPCPort          uint16
-	AccountGRPCAPIKey        string `envconf:"optional"`
 	HealthCheckPort          uint16 `envconf:"optional"`
 	EnableBella              bool   `envconf:"optional"`
 }
