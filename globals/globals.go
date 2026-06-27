5e9a1e98fc99fd000f5b9146233d93d037557b06
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-28 03:07:35 +0900

    Update globals.go

--- globals/globals.go
+++ globals/globals.go
@@ -2,7 +2,6 @@ package globals
 
 import (
 	"github.com/ss2552/3ds-friend/types"
-	pb "github.com/PretendoNetwork/grpc/go/account/v2"
 	"github.com/PretendoNetwork/nex-go/v2"
 	"github.com/PretendoNetwork/plogger-go"
 )
